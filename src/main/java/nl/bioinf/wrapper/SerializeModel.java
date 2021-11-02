938335c572292932cd3d533eece00158a6d0bbd8
JurrienDeJong <ju.de.jong@st.hanze.nl>
2021-11-02 09:06:37 +0100

    Changed some exceptions, to catch errors better

--- src/main/java/nl/bioinf/wrapper/SerializeModel.java
+++ src/main/java/nl/bioinf/wrapper/SerializeModel.java
@@ -3,7 +3,7 @@ package nl.bioinf.wrapper;
 import weka.classifiers.trees.J48;
 
 public class SerializeModel {
-    protected void saveModel(String modelFile, J48 j48) throws Exception {
-        weka.core.SerializationHelper.write(modelFile, j48);
+    protected void saveModel(String modelFile, J48 j48) throws Exception{
+            weka.core.SerializationHelper.write(modelFile, j48);
     }
 }
--- src/main/java/nl/bioinf/wrapper/WekaEngine.java
+++ src/main/java/nl/bioinf/wrapper/WekaEngine.java
@@ -3,8 +3,6 @@ package nl.bioinf.wrapper;
 import weka.classifiers.trees.J48;
 import weka.core.Instances;
 
-import java.io.IOException;
-
 public class WekaEngine {
     public static void main(String[] args) {
         WekaEngine runner = new WekaEngine();
@@ -19,17 +17,17 @@ public class WekaEngine {
         try {
             Instances data = fileReader.loadDataFiles(known_instances);
             Instances unknown_data = fileReader.loadDataFiles(unknown_instances);
-        } catch (IOException e){
+            ConstructTree j48 = new ConstructTree();
+            J48 tree = j48.buildTree(data);
+            SerializeModel save = new SerializeModel();
+            save.saveModel(model_file, tree);
+            DeserializeModel load = new DeserializeModel();
+            J48 model = load.loadModel(model_file);
+            InstanceClassifier Classifier = new InstanceClassifier();
+            Classifier.classifyNewInstances(model, unknown_data);
+
+        } catch (Exception e){
             System.out.println(e.getMessage());
         }
-            //ConstructTree j48 = new ConstructTree();
-            //J48 tree = j48.buildTree(data);
-            //SerializeModel save = new SerializeModel();
-            //save.saveModel(model_file, tree);
-            //DeserializeModel load = new DeserializeModel();
-            //J48 model = load.loadModel(model_file);
-
-            //InstanceClassifier Classifier = new InstanceClassifier();
-            //Classifier.classifyNewInstances(model, unknown_data);
     }
 }
